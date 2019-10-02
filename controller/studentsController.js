970ba6a4097bc216b4341f3ce289133b385d6d85
Emre Mert <emremycom@gmail.com>
2019-10-03 01:37:14 +0300

    03.10.2019 | component çalışma gerçekleştirildi v1.0.0.3

--- controller/studentsController.js
+++ controller/studentsController.js
@@ -1,6 +1,18 @@
+const student = {
+    name:"Emre mert yilmaz",
+    tcno:20000000000,
+    motherName:"Emrenin annesi",
+    motherNumber:10000000000,
+    fatherName:"Emrenin babasi",
+    fatherNumber:30000000000,
+    relative:"Abisi",
+    relativeNumber:60000000000
+}
+
 exports.index = (req,res,next)=>{
     res.render('studentsPage/index',{
         title:"Öğrenci işleri - AİDAT TAKİP SİSTEMİ",
-        path:"/aidatlar"
+        path:"/aidatlar",
+        student:student
     });
 }
