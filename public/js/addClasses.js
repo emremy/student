d98f9c45ea9b1175ab86419e136813bb60856215
Emre mert yilmaz <emremycom@gmail.com>
2019-10-03 15:29:56 +0200

    addClasses update

--- public/js/addClasses.js
+++ public/js/addClasses.js
@@ -1,5 +1,11 @@
-const element = document.getElementById('AddClasses');
+const AddClasses = document.querySelector('.AddClasses');
+const Panel = document.querySelector('#AddClassesPanel');
+const Close = document.querySelector('.close');
 
-document.addEventListener('select', element)(e =>{
-    console.log(e);
-});
+AddClasses.addEventListener('click', (e =>{
+    Panel.style.display = "flex";
+}))
+
+Close.addEventListener('click', (e=>{
+    Panel.style.display = "none";
+}))
