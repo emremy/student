b856c2dfb30c50ba5c220229947a8e63eb16d79b
emre mert yilmaz <emremycom@gmail.com>
2019-10-16 16:53:49 +0300

    hadi bakalım degisiklik olsun

--- controller/indexController.js
+++ controller/indexController.js
@@ -1,6 +1,18 @@
+const classes = [
+    {
+        id:1,
+        name:"5 / A"
+    },
+    {
+        id:2,
+        name:"5 / B"
+    }
+];
+
 exports.index = (req,res,next)=>{
     res.render('index/index',{
         title:"Anasayfa - AİDAT TAKİP SİSTEMİ",
-        path:"/"
+        path:"/",
+        classes:classes
     });
 }
--- controller/studentsController.js
+++ controller/studentsController.js
@@ -1,22 +1,37 @@
-const student = {
-    name:"Emre mert yilmaz",
-    tcno:20000000000,
-    motherName:"Emrenin annesi",
-    motherNumber:10000000000,
-    fatherName:"Emrenin babasi",
-    fatherNumber:30000000000,
-    relative:"Abisi",
-    relativeNumber:60000000000
-}
+var student = [
+    {
+        id:1,
+        name:"Emre mert yilmaz",
+        tcno:123456789,
+        motherName:"Emrenin annesi",
+        motherNumber:542312,
+        fatherName:"Emrenin babasi",
+        fatherNumber:512354,
+        relative:"Abisi",
+        relativeNumber:5341
+    },
+    {
+        id:2,
+        name:"Halil yilmaz",
+        tcno:123565788,
+        motherName:"Halilin annesi",
+        motherNumber:53412,
+        fatherName:"Halilin babasi",
+        fatherNumber:51235,
+        relative:"Abisi",
+        relativeNumber:52136
+    }
+];
 
 exports.index = (req,res,next)=>{
     res.render('studentsPage/index',{
         title:"Öğrenci işleri - AİDAT TAKİP SİSTEMİ",
         path:"/aidatlar",
-        student:student
+        student:student[0]
     });
 }
 exports.students = (req,res,next)=>{
+
     res.render('studentsPage/classToStudents',{
         title:"Şu sınıfa ait öğrenciler",
         path:"/ogrenciler",
